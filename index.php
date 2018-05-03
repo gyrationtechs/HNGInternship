5779298169b0d075d41c6733f18361122a5295a5
David Ozokoye <davidozokoye@gmail.com>
2018-05-03 03:41:51 +0100

    HNG Design
    Html and css implementation of Figma design

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+
+<html lang="en-US">
+<head>
+	<meta charset="utf-8">
+	<title>HNG Internship Design</title>
+	<link href="files/css/style.css" rel="stylesheet" />
+<style>
+body {
+	background-image: url("files/img/bg/bg-1.jpg");
+}
+</style>
+</head>
+
+<body>
+<div class="link">
+	<ul>
+		<li><a href="#home"> Home   |  </a></li>
+		<li><a href="#blog">  |  Blog   |  </a></li>
+		<li><a href="#about">  |  About  |  </a></li>
+		<li><a href="#contact">  |  Contact  </a></li>
+
+  </ul>
+</div>
+<div class="logo">
+
+<h1><span>HNG</span> Internship <span>4</span></h1>
+<?php
+			$date = date("d M, Y h:i A");
+			print('<p class="date">'.$date.'</p>');
+		?>
+
+</div>
+
+<div class="footer">
+<p>	2018 © HNG Internship 4 - All Rights Reserved</p>
+  <p>Designed By <b style="color: #1C58CC;"><a href="https://gyrationtechs.com.ng">David</a></b>
+</p>
+</div>
+
+
+</body>
+</html>
